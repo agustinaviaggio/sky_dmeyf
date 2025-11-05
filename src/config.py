423b1d45ee801835d0c4cac70a8087e87b8a8434
agustinaviaggio <agustinaviaggio@gmail.com>
2025-11-05 11:20:51 -0300

    fix config.py bug

--- src/config.py
+++ src/config.py
@@ -12,16 +12,16 @@ try:
         _cfgGeneral = yaml.safe_load(f)
         _cfg = _cfgGeneral["competencia02"]
 
-        STUDY_NAME = _cfgGeneral("STUDY_NAME")
-        DATA_PATH = _cfg("DATA_PATH")
-        OUTPUT_PATH = _cfg("OUTPUT_PATH")
-        SQL_TABLE_NAME = ('SQL_TABLE_NAME')
-        SEMILLA = _cfg("SEMILLA")
-        MESES_TRAIN = _cfg("MESES_TRAIN")
-        MES_VALIDACION = _cfg("MES_VALIDACION")
-        MES_TEST = _cfg("MES_TEST")
-        GANANCIA_ACIERTO = _cfg("GANANCIA_ACIERTO")
-        COSTO_ESTIMULO = _cfg("COSTO_ESTIMULO")
+        STUDY_NAME = _cfgGeneral["STUDY_NAME"]
+        DATA_PATH = _cfg["DATA_PATH"]
+        OUTPUT_PATH = _cfg["OUTPUT_PATH"]
+        SQL_TABLE_NAME = ['SQL_TABLE_NAME']
+        SEMILLA = _cfg["SEMILLA"]
+        MESES_TRAIN = _cfg["MESES_TRAIN"]
+        MES_VALIDACION = _cfg["MES_VALIDACION"]
+        MES_TEST = _cfg["MES_TEST"]
+        GANANCIA_ACIERTO = _cfg["GANANCIA_ACIERTO"]
+        COSTO_ESTIMULO = _cfg["COSTO_ESTIMULO"]
 
 except Exception as e:
     logger.error(f"Error al cargar el archivo de configuracion: {e}")
