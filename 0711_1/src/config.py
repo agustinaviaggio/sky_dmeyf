439b4db07117fc1b11af77a68e09d9f96b1a9312
agustinaviaggio <agustinaviaggio@gmail.com>
2025-11-07 12:48:19 -0300

    act

--- 0711_1/src/config.py
+++ 0711_1/src/config.py
@@ -10,16 +10,13 @@ PATH_CONFIG = os.path.join(os.path.dirname(os.path.dirname(__file__)), "conf.yam
 try:
     with open(PATH_CONFIG, "r") as f:
         _cfgGeneral = yaml.safe_load(f)
-        _cfg = _cfgGeneral["01_FE"]
+        _cfg = _cfgGeneral["configuracion"]
 
         STUDY_NAME = _cfgGeneral["STUDY_NAME"]
         DATA_PATH = _cfg["DATA_PATH"]
-        BUCKET_NAME = _cfg["BUCKET_NAME"]
         OUTPUT_PATH = _cfg["OUTPUT_PATH"]
         SQL_TABLE_NAME = _cfg['SQL_TABLE_NAME']
         SEMILLAS = _cfg["SEMILLAS"]
-        GANANCIA_ACIERTO = _cfg["GANANCIA_ACIERTO"]
-        COSTO_ESTIMULO = _cfg["COSTO_ESTIMULO"]
 
 except Exception as e:
     logger.error(f"Error al cargar el archivo de configuracion: {e}")
